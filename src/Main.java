6b41acd50370d38f3b638bde37e8f5ff52344d65
group <galin_psy@abv.bg>
2016-02-23 02:50:35 +0200

    Home work

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+        String firstWord = "Wanna";
+        String secondWord = "sleep";
+        System.out.println(firstWord + " " + secondWord);
+
+        
+
+	}
+}
